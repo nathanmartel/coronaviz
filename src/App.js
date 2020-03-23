7cd23155151b895ff3d77c60c22f63f203c0c27d
Nathan Martel <nathanmartel@gmail.com>
2020-03-23 01:25:28 -0700

    graphing local data, fetch in place

--- src/App.js
+++ src/App.js
@@ -1,55 +1,98 @@
 import React, { Component, useRef, useEffect, useState } from 'react';
 import request from 'superagent';
 import './App.css';
-import Main from './Main';
-import { select, line } from 'd3';
+import JSONdata from './daily.json'
+import { select, line, curveCardinal, axisBottom, axisRight, scaleLinear } from 'd3';
+
+function formatDate(badDate) {
+  return badDate.toString().slice(5,6) + '/' + badDate.toString().slice(6);
+}
 
 function App() {
   
-  // state = {
-  //   data: []
-  // }
+  // const covidData = getData();
+  const [positiveData, setPositiveData] = useState([JSONdata.map(item => item.positive)]);
+  const [dateData, setDateData] = useState(JSONdata.map(item => item.date));
+  // setDateData(JSONdata.map(item => item.date));
 
-  
-  // componentDidMount = async() => {
-  //   const result = await request.get('https://covidtracking.com/api/us/daily');
-  //   console.log(result);
-  //   this.setState({ data: result.text });
-  // };
+  console.log('PositiveData: ', positiveData)
 
-  const [tempData, setData] = useState([25, 30, 45, 60, 20]);
   const svgRef = useRef();
 
-  useEffect(() => {
+  useEffect( () => {
+    
+    const fetchData = async() => {
+      const result = await request.get('https://covidtracking.com/api/us/daily');
+      const resultObj = JSON.parse(result.text);
+      console.log('Data is:', resultObj);
+      setPositiveData(resultObj.map(item => item.positive));
+      setDateData(resultObj.map(item => item.date));
+    }
+
+    // Looks like I'm running a loop here, let's just stick with local data...
+    fetchData();
+
     const svg = select(svgRef.current);
-    const myLine = line()
-      .x((value, index) => index * 50)
-      .y(value => 150 - value);
+    
+    const xScale = scaleLinear()
+      .domain([0, positiveData.length - 1])
+      .range([0, 600]);
+    const yScale = scaleLinear()
+      .domain([0, Math.max(...positiveData)])
+      .range([400, 0]);
+  
+    const xAxis = axisBottom(xScale)
+      .ticks(positiveData.length)
+      .tickFormat(index => formatDate(dateData[index]));
+    const yAxis = axisRight(yScale)
+      .ticks(positiveData.length);
 
     svg
-      .selectAll('path')
-      .data([tempData])
+      .select('.x-axis')
+      .style('transform', 'translateY(400px)')
+      .call(xAxis);
+
+    svg
+      .select('.y-axis')
+      .style('transform', 'translateX(600px)')
+      .call(yAxis);
+
+    
+    const myLine = line()
+      .x((value, index) => xScale(index))
+      .y(yScale)
+      .curve(curveCardinal);
+
+    svg
+      .selectAll('.line')
+      .data([positiveData])
       .join('path')
+      .attr('class', 'line')
       .attr('d', value => myLine(value))
       .attr('fill', 'none')
       .attr('stroke', 'blue');
-  }, [tempData]);
+  }, []);
 
   return (
     <React.Fragment>
     <div className="App">
       <header className="App-header">
-        <p>
-          Coronavirus visualizations.
-        </p>
+        <h1>
+          Coronavirus Visualizations
+        </h1>
       </header>
       <div>
-        {/* <Main data={this.state.data} /> */}
-
-      <svg ref={svgRef}></svg>
+      <h2>Total U.S. Positive Cases</h2>
+      <svg ref={svgRef}>
+        <g className='x-axis' />
+        <g className='y-axis' />
+      </svg>
       <br />
-      <button onClick={() => setData(tempData.map(value => value + 5))}>Update Data</button>
-      <button onClick={() => setData(tempData.filter(value => value < 35))}>Filter Data</button>
+      <br />
+      <br />
+      <br />
+      {/* <button onClick={() => setData(tempData.map(value => value + 5))}>Update Data</button>
+      <button onClick={() => setData(tempData.filter(value => value < 35))}>Filter Data</button> */}
       </div>
     </div>
     </React.Fragment>
